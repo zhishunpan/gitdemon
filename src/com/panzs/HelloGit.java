d6007e7585f4b7b97cbe0d1066193d8211a9a2f2
zhishunpan <171888430@qq.com>
2015-07-03 15:39:28 +0800

    第二次提交

--- src/com/panzs/HelloGit.java
+++ src/com/panzs/HelloGit.java
@@ -1,7 +1,14 @@
 package com.panzs;
-
+/**
+ * 
+ * <描述这个类/接口的负责什么>
+ *
+ * @author 潘志顺
+ * @date 创建时间：2015年7月3日 下午3:38:49
+ */
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git");
+		System.out.println("测试的了");
 	}
 }
